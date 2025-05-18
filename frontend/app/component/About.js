fafc992a7b00df9665fae2544b528b517272834b
umang <145416860+UMANG-2003@users.noreply.github.com>
2025-05-18 20:10:58 +0530

    Update About.js

--- frontend/app/component/About.js
+++ frontend/app/component/About.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Image from 'next/image'
 
 function About() {
   return (
@@ -19,13 +18,7 @@ function About() {
             <div>
                 <button className='flex items-center gap-2 bg-pink-300 text-black rounded-full px-4 py-2 mt-5 cursor-pointer'>
                     <a href="https://www.instagram.com/hidden_snaps_/" target="_blank" rel="noopener noreferrer">
-                        <Image
-                          src="https://cdn-icons-png.flaticon.com/512/87/87390.png"
-                          alt=""
-                          width={20}
-                          height={20}
-                          className='w-[20px] h-[20px]'
-                        />
+                        <img src="https://cdn-icons-png.flaticon.com/512/87/87390.png" alt="" className='w-[20px] h-[20px]' />
                     </a>
                     <span className='text-black text font-bold'>Instagram</span>
                 </button>
@@ -34,30 +27,9 @@ function About() {
 
         <div className='bg-black bg2 w-[70%] h-[70vh] max-md:h-[100%] max-md:w-full rounded-lg'>
             <div className='flex flex-wrap justify-around items-center h-full p-5'>
-                <Image
-                  src="https://images.pexels.com/photos/2026960/pexels-photo-2026960.jpeg?auto=compress&cs=tinysrgb&w=600"
-                  alt=""
-                  width={600 * 0.25}
-                  height={600 * 0.25}
-                  className='w-[25%] max-md:h-[45%] max-md:w-[30%] rounded-xl'
-                  style={{ objectFit: 'cover' }}
-                />
-                <Image
-                  src="https://images.pexels.com/photos/2130795/pexels-photo-2130795.jpeg?auto=compress&cs=tinysrgb&w=600"
-                  alt=""
-                  width={600 * 0.25}
-                  height={600 * 0.25}
-                  className='w-[25%] max-md:h-[45%] max-md:w-[30%] rounded-xl'
-                  style={{ objectFit: 'cover' }}
-                />
-                <Image
-                  src="https://images.pexels.com/photos/1882309/pexels-photo-1882309.jpeg?auto=compress&cs=tinysrgb&w=600"
-                  alt=""
-                  width={600 * 0.25}
-                  height={600 * 0.25}
-                  className='w-[25%] max-md:h-[45%] max-md:w-[30%] rounded-xl'
-                  style={{ objectFit: 'cover' }}
-                />
+                <img src="https://images.pexels.com/photos/2026960/pexels-photo-2026960.jpeg?auto=compress&cs=tinysrgb&w=600" className='w-[25%] max-md:h-[45%] max-md:w-[30%]  rounded-xl' alt="" />
+                <img src="https://images.pexels.com/photos/2130795/pexels-photo-2130795.jpeg?auto=compress&cs=tinysrgb&w=600" className='w-[25%] max-md:h-[45%] max-md:w-[30%] rounded-xl' alt="" />
+                <img src="https://images.pexels.com/photos/1882309/pexels-photo-1882309.jpeg?auto=compress&cs=tinysrgb&w=600" className='w-[25%] max-md:h-[45%] max-md:w-[30%] rounded-xl' alt="" />
             </div>
         </div>
       </div>
